9c3c6dd8fa633282827d6d4dcaf04ed36adb29fb
Babyface113 <dragitancev@gmail.com>
2015-02-27 13:00:02 +0100

    php

---
+++ c8.php
@@ -0,0 +1,132 @@
+<?php
+
+// function zdravo(){
+// 	echo "Zdravo";
+// 	echo "<br/>";
+// }
+
+// zdravo();
+// zdravo();
+// zdravo();
+// zdravo();
+// zdravo();
+// zdravo();
+// zdravo();
+// zdravo();
+
+// function Pero(){
+// 	echo "Pero nash golem maz";
+// }
+
+// Pero();
+
+
+
+
+// function mleko($tip){
+// 	echo $tip;
+// }
+// mleko('Bitolsko');
+// mleko('Tursko');
+
+
+// function sobiranje($a, $b){
+// 	echo "<br/>";
+// 	echo $a + $b;
+// }
+
+// sobiranje(2, 6);
+// sobiranje(45, 68);
+// sobiranje(549, 123333);
+
+// echo "<hr/>";
+
+// function odzemanje($c, $d){
+// 	echo "<br/>";
+// 	echo $c - $d;
+// }
+
+
+// odzemanje(9, 7);
+// odzemanje(22, 11);
+// odzemanje(50, 51);
+
+// echo "<hr/>";
+
+// function mnozenje($e, $f){
+// 	echo "<br/>";
+// 	echo $e * $f;
+// }
+
+// mnozenje(3, 3);
+// mnozenje(10, 13);
+// mnozenje(15, 20);
+
+// echo "<hr/>";
+
+// function delenje($g, $h){
+// 	echo "<br/>";
+// 	echo $g / $h;
+// }
+
+// delenje(20, 5);
+// delenje(120, 13);
+// delenje(250, 25);
+
+// echo "<hr/>";
+
+// function pozdrav($ime = 'Dragi'){
+// 	echo "Zdravo ".$ime;
+// }
+
+// pozdrav('Pero');
+
+// echo "<hr/>";
+
+// function imee($ime){
+// 	$broj = strlen($ime);
+// 	if($broj % 2 == 0){
+// 		echo "paren broj";
+// 	}else{
+// 		echo "neparen broj";
+// 	}
+// }
+
+// imee('Dragi');
+
+
+
+// function sob($a, $b){
+// 	return $a + $b;
+// }
+
+// $rezultat = sob(2, 5);
+// echo $rezultat;
+
+// echo sob(2,3);
+
+
+function odzemanje($c, $d){
+	return $c - $d;
+
+}
+echo odzemanje(9,7);
+
+echo "<hr/>";
+
+function mnozenje($e, $f){
+	return $e * $f;
+}
+
+echo mnozenje(4, 6);
+
+echo "<hr/>";
+
+function delenje($g, $h){
+	return $g / $h;
+}
+
+echo delenje(28, 4);
+
+
+?>
