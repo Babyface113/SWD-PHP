5d1e04d451d2084ee2fedacdc6fad549cb049545
Babyface113 <dragitancev@gmail.com>
2015-03-20 13:12:44 +0100

    php

---
+++ c12.php
@@ -0,0 +1,47 @@
+<?php
+
+
+// $gradovi = array('Gevgelija', 'Skopje', 'Veles', 'Bitola', 'Prilep');
+
+// $broj = count($gradovi);
+
+// for($i = 0; $i <$broj; $i++){
+// 	echo $grad[$i];
+// 	echo "<br>";
+// }
+
+
+
+// $student = array('ime' => 'Dragi', 'prezime' => 'Tanchev', 'prosek' => 10.0);
+
+// foreach ($student as $key => $info) {
+// 	echo '<b>'.$key.'</b>: ';
+// 	echo $info;
+// 	echo "<br>";
+// }
+
+// foreach ($gradovi as $grad) {
+// 	$dolzina = strlen($grad);
+// 	if($dolzina %2 == 0){
+// 		echo $grad;
+// 		echo "<br>";
+// 	}
+	
+// }
+
+
+$a = 0;
+
+while($a < 50){
+
+	$a++;
+
+	if($a % 2 == 1){
+		echo $a;
+		echo "<br>";
+	}
+}
+
+
+
+?>
